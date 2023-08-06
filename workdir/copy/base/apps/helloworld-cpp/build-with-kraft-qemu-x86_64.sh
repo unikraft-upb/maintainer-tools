4c0a2960eac5c9c2254459be428f2c19616f4293
Razvan Deaconescu <razvand@unikraft.io>
2023-08-06 20:39:56 +0300

    apps: Fix path to kraftkit for helloworld-cpp
    Fix wrong path (`~/kraftkit/dist/kraft`) in the build script. New path
    is simply `kraft`.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- workdir/copy/base/apps/helloworld-cpp/build-with-kraft-qemu-x86_64.sh
+++ workdir/copy/base/apps/helloworld-cpp/build-with-kraft-qemu-x86_64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -fr .unikraft/build
-~/kraftkit/dist/kraft build --log-level debug --log-type basic --plat qemu --arch x86_64 -j $(nproc)
+kraft build --log-level debug --log-type basic --plat qemu --arch x86_64 -j $(nproc)
