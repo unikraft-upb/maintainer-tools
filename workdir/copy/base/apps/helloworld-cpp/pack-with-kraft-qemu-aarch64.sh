49cd8fcb244f8ca11f01f42a381c154053546d38
Razvan Deaconescu <razvand@unikraft.io>
2023-08-06 18:40:09 +0300

    copy: Add scripts for helloworld-cpp app
    Add build, run and kraft-related scripts for helloworld-cpp app.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ workdir/copy/base/apps/helloworld-cpp/pack-with-kraft-qemu-aarch64.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RUNTIME_DIR=$HOME/.local/kraftkit/runtime
+REPO_NAME=helloworld-cpp-qemu-aarch64:latest
+
+rm -fr $RUNTIME_DIR
+mkdir $RUNTIME_DIR
+
+kraft pkg --runtime-dir $RUNTIME_DIR --as oci --name index.unikraft.io/unikraft.org/$REPO_NAME --with-kconfig
