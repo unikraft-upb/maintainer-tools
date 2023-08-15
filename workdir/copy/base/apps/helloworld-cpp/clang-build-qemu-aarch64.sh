4a31c72c18e210227693d2b043a0384008cac853
Razvan Deaconescu <razvand@unikraft.io>
2023-08-15 14:47:22 +0300

    copy: Remove helloworld-cpp build and run scripts
    Scripts are to be replaced by generated scripts after refactoring, i.e.
    using `workdir/copy/common-nocopy/generate.sh`.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- workdir/copy/base/apps/helloworld-cpp/clang-build-qemu-aarch64.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-make distclean
-config=$(ls $(pwd)/.config.*-qemu-aarch64)
-UK_DEFCONFIG=$config make defconfig
-make CC=clang LD=aarch64-linux-gnu-gcc OBJCOPY=aarch64-linux-gnu-objcopy STRIP=aarch64-linux-gnu-strip prepare
-make CC=clang LD=aarch64-linux-gnu-gcc OBJCOPY=aarch64-linux-gnu-objcopy STRIP=aarch64-linux-gnu-strip -j $(nproc)
--- workdir/copy/base/apps/helloworld-cpp/run-fc-x86_64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm /tmp/firecracker.socket
-firecracker-x86_64 \
-    --api-sock /tmp/firecracker.socket \
-    --config-file helloworld-cpp-fc-x86_64.json
